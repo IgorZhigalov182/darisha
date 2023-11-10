8e002ffe44323021a0302b20f3db7a84f70adec2
Igor <zhigalovzenit112233@gmail.com>
2023-11-10 14:02:50 +0300

    basic proto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import './App.css';
+import '../src/styles/App.css';
+import About from './layouts/about/About';
+import Contact from './layouts/contact/Contact';
+import Cover from './layouts/cover/Cover';
+import Works from './layouts/works/Works';
 
 function App() {
-  return <>Darisha</>;
+  return (
+    <>
+      <Cover />
+      <About />
+      <Works />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
