7f8461bf2d21f9bd2cf267482fda2f05f015f31c
Geetork <galinaleespb@gmail.com>
2023-11-24 19:56:52 +0300

    animation of modals added

---
+++ src/layouts/gallery/ImageModal.jsx
@@ -0,0 +1,90 @@
+import ReactDOM from 'react-dom';
+import styles from './Gallery.module.scss';
+import Arrows from './Arrows';
+import SideMenu from './SideMenu';
+import { useEffect, useState } from 'react';
+
+const InfoModal = ({ setShowInfoModal, children }) => {
+    const [visible, setVisible] = useState(true);
+
+    const onClose = async () => {
+        setVisible(false);
+        setTimeout(() => {
+            setShowInfoModal(false);
+        }, 1000);
+    }
+
+    return (
+        <div id='modal' className={`${styles.info_modal_container}
+            ${visible ? styles.info_modal_show : styles.info_modal_hide}`}>
+            <div onClick={onClose} className={styles.close_button}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="#5E5E5E" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+            </div>
+            <div>
+                {children}
+            </div>
+        </div>
+    );
+}
+
+const modalRoot = document.getElementById("imageModal"); 
+
+const ImageModal = ({ data, currentData, topPosition, onClose }) => {
+    const [currentPic, setCurrentPic] = useState(data.flat().findIndex(src => src === currentData.src));
+    const [translateX, setTranslateX] = useState(`translateX(-${currentPic * 100}%)`);
+    const picNumber = data.flat().length;
+
+    const [showInfoModal, setShowInfoModal] = useState(false);
+    const [showModal, setShowModal] = useState(true);
+
+    const onCloseWithAnimation = async () => {
+        setShowModal(false);
+        setTimeout(() => {
+            onClose();
+        }, 100);
+    }; 
+
+    const previousPic = () => {
+        currentPic === 0 ?
+            setTranslateX(`translateX(0)`) :
+            setTranslateX(`translateX(-${(currentPic - 1) * 100}%)`);
+
+        currentPic === 0 ?
+            setCurrentPic(picNumber - 1) :
+            setCurrentPic(currentPic - 1);
+    };
+
+    const nextPic = () => {
+        currentPic === picNumber - 1 ?
+            setTranslateX(`translateX(0)`) :
+            setTranslateX(`translateX(-${(currentPic + 1) * 100}%)`);
+
+        currentPic === picNumber - 1 ?
+            setCurrentPic(0) :
+            setCurrentPic(currentPic + 1);
+    };
+
+    return ReactDOM.createPortal((
+        <div
+            className={`${styles.modal_container}
+            ${showModal ? styles.modal_container_show : styles.modal_container_hide}`}
+            style={{ top: topPosition }}>
+            <div className={styles.carousel}>
+                {
+                    data.flat().map((src, index) => <img style={{ transform: translateX }} key={index}
+                        className={`${styles.slider_container} ${styles.modal_img}`} src={src} alt="gallery" />
+                    )
+                }
+            </div>
+            <SideMenu onClose={onCloseWithAnimation} showInfoModal={showInfoModal} setShowInfoModal={setShowInfoModal}/>
+            <Arrows next={nextPic} previous={previousPic} />
+            {
+                showInfoModal && <InfoModal setShowInfoModal={setShowInfoModal}></InfoModal>
+            }
+        </div>
+    ), modalRoot);
+}
+
+export default ImageModal;
---
+++ src/layouts/gallery/SideMenu.jsx
@@ -0,0 +1,22 @@
+import styles from './Gallery.module.scss';
+
+const SideMenu = ({ onClose, setShowInfoModal, setShowModal }) => {
+    const showHideInfoModal = () => setShowInfoModal(true);
+
+    return (
+        <div className={styles.sidemenu_container}>
+            <div className={styles.sidemenu_item} onClick={onClose}>
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#5f5f5f" viewBox="0 0 18 18">
+                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                </svg>
+            </div>
+            <div className={styles.sidemenu_item} onClick={showHideInfoModal}>
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#5f5f5f" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+            </div>
+        </div>
+    );
+};
+
+export default SideMenu;
