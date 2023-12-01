7603be1d632b6668220e8e23e09fc4c3be182703
Galina Li <58363643+Geetork@users.noreply.github.com>
2023-12-01 10:33:42 +0300

    Update SideMenu.jsx

--- src/layouts/gallery/SideMenu.jsx
+++ src/layouts/gallery/SideMenu.jsx
@@ -6,8 +6,8 @@ const SideMenu = ({ onClose, setShowInfoModal, setShowModal }) => {
     return (
         <div className={styles.sidemenu_container}>
             <div className={styles.sidemenu_item} onClick={onClose}>
-                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#5f5f5f" viewBox="0 0 18 18">
-                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#5f5f5f" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
                 </svg>
             </div>
             <div className={styles.sidemenu_item} onClick={showHideInfoModal}>
